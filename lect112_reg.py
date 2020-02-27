4ddcca1449728bb164abc490f887f1ceeee7fc50
harshitksrivastava <harsh0311@gmail.com>
2020-02-28 01:12:01 +0530

    added lect_calendar & lect_permutations

--- lect112_reg.py
+++ lect112_reg.py
@@ -1,17 +1,17 @@
 import re
 
-patterns = ['term1','term2']
+patterns = ['term1', 'term2']
 
 text = 'This is a string with term1, not not the other!'
 
 for pattern in patterns:
-    print("I'm searching for: "+pattern)
-# pattern in re.search(what_we_are_looking_for, in_what_we_are_looking_for)
-    if( re.search(pattern,text)):
+    print("I'm searching for: " + pattern)
+    # pattern in re.search(what_we_are_looking_for, in_what_we_are_looking_for)
+    if re.search(pattern, text):
         print('Match!')
     else:
         print("No Match!")
     # match = re.search('term1',text)
     # print(match.start())
-#========================================================
-print(re.findall('match','test phrase match in match midle'))
+# ========================================================
+print(re.findall('match', 'test phrase match in match midle'))
---
+++ lect_calendar.py
@@ -0,0 +1,15 @@
+
+import calendar
+
+# technique 1
+weekdays = {1: 'MONDAY', 2: 'TUESDAY', 3: 'WEDNESDAY', 4: 'THURSDAY', 5: 'FRIDAY', 6: 'SATURDAY', 0: 'SUNDAY'}
+day, month, year = map(int, input().split())
+# calendar.TextCalendar.setfirstweekday(6)
+print(calendar.TextCalendar(6).formatweekday((calendar.weekday(year, month, day)), width=9).upper())
+
+#
+# # technique 2
+# list_week = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY']
+# list_week1 = list(enumerate(list_week, start=0))
+# day, month, year = map(int, input().split())
+# print(list_week1[(calendar.weekday(year, month, day))][1])
